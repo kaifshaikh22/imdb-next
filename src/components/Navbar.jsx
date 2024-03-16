7055f3f71df38ebc591042bd160fa633e606da0e
kaif shaikh <kaaifshaikh22@gmail.com>
2024-03-16 16:07:21 +0530

    create the navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import NavbarItem from "./NavbarItem";
+
+
+export default function Navbar() {
+  return (
+    <div className="flex dark:bg-gray-600 bg bg-amber-200 p-4 lg:text-lg
+    justify-center gap-6">
+        <NavbarItem title="Trending"
+        param="fetchTrending" />
+        <NavbarItem title="Top Rated"
+        param="fetchTopRated" />
+        <NavbarItem />
+    </div>
+  )
+}
