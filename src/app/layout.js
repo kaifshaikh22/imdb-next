0b60f1c32d12c6f1605bd7a188a482a797cf7820
kaif shaikh <kaaifshaikh22@gmail.com>
2024-03-16 22:12:05 +0530

    updated scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,8 @@ import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
 
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
