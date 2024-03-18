a5027c0f21ee14dc9d629ea6e8190b9fd0595445
kaif shaikh <kaaifshaikh22@gmail.com>
2024-03-18 13:25:02 +0530

    updated l

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,44 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Providers from "./Providers";
-import Navbar from "@/components/Navbar";
-import SearchBox from "@/components/SearchBox";
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+import Header from '@/components/Header';
+
+import Providers from './Providers';
+
+import Navbar from '@/components/Navbar';
+
+import SearchBox from '@/components/SearchBox';
 
 
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({ subsets: ['latin'] });
+
+
 
 export const metadata = {
-  title: "IMDB clone",
-  description: "This is a movie database clone",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
+
         <Providers>
-        <Header />
-        <Navbar />
-        <SearchBox />
-        {children}
+
+          <Header />
+
+          <Navbar />
+
+          <SearchBox />
+
+          {children}
+
         </Providers>
-        </body>
+
+      </body>
     </html>
   );
 }
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SearchBox() {
   const [search, setSearch] = useState('');
@@ -9,22 +9,25 @@ export default function SearchBox() {
   const handleSubmit = (e) => {
     e.preventDefault();
     router.push(`/search/${search}`);
-    
   };
   return (
-    <form className="flex justify-between px-5 max-w-6xl mx-auto" onSubmit={handleSubmit}>
-        <input 
+    <form
+      className='flex justify-between px-5 max-w-6xl mx-auto'
+      onSubmit={handleSubmit}
+    >
+      <input
         type='text'
-        placeholder="Search keywords..."
-        className="w-full h-14 rounded-md placeholder-gray-500 outline-none
-        bg-transparent flex-1"
+        placeholder='Search keywords...'
+        className='w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        />
-        <button className="text-amber-600 disabled:text-gray-400" disabled={search === ''}>
-            Search
-        </button>
+      />
+      <button
+        className='text-amber-600 disabled:text-gray-400'
+        disabled={search === ''}
+      >
+        Search
+      </button>
     </form>
-    
   );
 }
